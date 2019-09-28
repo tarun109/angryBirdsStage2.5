c0756ded8251728a0189a838ed8bc054146fca39
tarun109 <54740077+tarun109@users.noreply.github.com>
2019-09-28 21:02:08 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,20 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:10
+            
+        }  
+        this.pointB=pointB;
+    this.sling=Constraint.create(options);
+    World.add(world,this.sling);       
+}
+display(){
+var pointA=this.sling.bodyA.position;
+var pointB=this.pointB;
+    strokeWeight(5);
+    line(pointA.x,pointA.y,pointB.x,pointB.y); 
+}
+}
